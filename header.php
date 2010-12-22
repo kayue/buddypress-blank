b17143639e4893adcb8cc73b59a7f0808c89bd96
Ka Yue <kayuey@gmail.com>
2010-12-21 21:31:29 -0500

    Add a better description meta tag

--- header.php
+++ header.php
@@ -3,17 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-        <?php
-            if(is_single() OR is_page()){
-                the_post();
-                $description = trim(strip_tags(str_replace(array("\n", '[...]'), ' ', get_the_excerpt())));
-                rewind_posts();
-            }
-            else{
-                $description = 'Default description';
-            }
-        ?>
-        <meta name="description" content="<?php echo $description; ?>">
+        <meta name="description" content="<?php echo get_trimmed_excerpt() ?>">
         <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
         <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?>" href="<?php bloginfo('rss2_url'); ?>">
         <link rel="shortcut icon" href="<?php bloginfo(url); ?>/favicon.ico" type="image/x-icon">
@@ -21,8 +11,7 @@
         <?php wp_head(); ?>
     </head>
     
-    <? if (is_single() && $post = $wp_query->post) { $cat_class = get_the_category(); $singleCategory = "single-".$cat_class[0]->slug; } // include category class ?>
-    <body <?php body_class($singleCategory) ?>>
+    <body <?php body_class() ?>>
     
     <div id="container">
         <div id="header">
