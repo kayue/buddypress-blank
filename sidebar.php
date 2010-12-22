47fdd4212aa534ba1fd92cfda8d224d43e7cbe31
Ka Yue <kayuey@gmail.com>
2010-12-21 22:58:32 -0500

    Include sidebar.

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,22 @@
 <div id="sidebar-container">
+    
+    <?php do_action( 'bp_before_sidebar' ) ?>
+    
     <div id="sidebar">
-        <h3>Sidebar</h3>
+        
+        <? do_action( 'bp_inside_before_sidebar' ); ?>
+    	
+    	<? if(bp_is_blog_page()):
+        	    dynamic_sidebar( 'primary-sidebar' );
+        	else:
+        	    dynamic_sidebar( 'community-sidebar' );
+        	endif;
+        ?>
+        
+        <? do_action( 'bp_inside_after_sidebar' ); ?>
+        
     </div>
+    
+    <?php do_action( 'bp_after_sidebar' ) ?>
+    
 </div>
