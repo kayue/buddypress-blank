297ea2b8fe7829e0bdc947485cd6f0981a3a3241
Ka Yue <kayuey@gmail.com>
2010-12-21 22:57:50 -0500

    Add category and archive page template.

---
+++ archive.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+
+<div id="content-container">
+    
+    <?php do_action( 'bp_before_archive' ) ?>
+    
+    <div id="content">
+        <? if ( have_posts() ) the_post(); ?>
+        <h1 class="page-title">
+            <? if ( is_day() ) : ?>
+            	<?php printf( "Daily Archives: <span>%s</span>", get_the_date() ); ?>
+            <? elseif ( is_month() ) : ?>
+            	<?php printf( "Monthly Archives: <span>%s</span>", get_the_date('F Y') ); ?>
+            <? elseif ( is_year() ) : ?>
+            	<?php printf( "Yearly Archives: <span>%s</span>", get_the_date('Y') ); ?>
+            <? else : ?>
+            	Blog Archives
+            <? endif; ?>
+		</h1>
+        <? rewind_posts(); ?>
+        
+        <? get_template_part( 'loop', 'home' ); ?>
+    </div> <!-- #content -->
+    
+    <?php do_action( 'bp_after_archive' ) ?>
+    
+</div> <!-- #content-container -->
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
---
+++ category.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+<div id="content-container">
+    
+    <div id="content">
+        <h1 class="page-title">
+            <? printf( "Category Archives: %s", '<span>' . single_cat_title( '', false ) . '</span>' ); ?>
+		</h1>
+        
+        <?php
+        	$category_description = category_description();
+        	if ( ! empty( $category_description ) )
+        		echo '<div class="category-description">' . $category_description . '</div>';
+        ?>
+        
+        <? get_template_part( 'loop', 'home' ); ?>
+    </div> <!-- #content -->
+    
+</div> <!-- #content-container -->
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
