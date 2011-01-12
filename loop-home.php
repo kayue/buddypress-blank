c1a65c549c944fe84ece13429096137d294f86a3
Ka Yue <kayuey@gmail.com>
2011-01-12 16:49:39 +0800

    Remove redundant div.

--- loop-home.php
+++ loop-home.php
@@ -6,22 +6,20 @@
         
 		<div class="post" id="post-<?php the_ID(); ?>">
 		
-			<div class="content">
-			    <p class="date"><?php the_time('l, j F Y'); ?> by <?=bp_core_get_userlink( $post->post_author )?></p>
-			    <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
-    				<h2 class="title"><?php the_title(); ?></h2>
-				</a>
+		    <p class="date"><?php the_time('l, j F Y'); ?> by <?=bp_core_get_userlink( $post->post_author )?></p>
+		    <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
+				<h2 class="title"><?php the_title(); ?></h2>
+			</a>
 
-				<div class="entry">
-					<?php the_content('Continue reading...'); ?>
-				</div>
-				
-				<div class="metadata">
-				    <p class="categories"><?php the_category(', ') ?></p>
-				    <p class="tags"><?php the_tags('', ', ', ''); ?></p>
-				    <p class="comments"><?php comments_popup_link( 'No Comments &#187;', '1 Comment &#187;', '% Comments &#187;' ); ?></p>
-				</div>
-			</div> <!-- / .content -->
+			<div class="entry">
+				<?php the_content('Continue reading...'); ?>
+			</div>
+			
+			<div class="metadata">
+			    <p class="categories"><?php the_category(', ') ?></p>
+			    <p class="tags"><?php the_tags('', ', ', ''); ?></p>
+			    <p class="comments"><?php comments_popup_link( 'No Comments &#187;', '1 Comment &#187;', '% Comments &#187;' ); ?></p>
+			</div>
 
 		</div> <!-- / .post -->
         
